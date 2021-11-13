c48fe41ac3b2aae2e76fce0e44ab0647530147ee
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2021-11-13 15:15:31 -0600

    Fix wrong certificate name

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 
 dependencies = [
-    "blspy==1.0.2",  # Signature library
-    "chiavdf==1.0.2",  # timelord and vdf verification
+    "blspy==1.0.6",  # Signature library
+    "chiavdf==1.0.3",  # timelord and vdf verification
     "chiabip158==1.0",  # bip158-style wallet filters
-    "chiapos==1.0.3",  # proof of space
+    "chiapos==1.0.4",  # proof of space
     "clvm==0.9.7",
-    "clvm_rs==0.1.8",
+    "clvm_rs==0.1.14",
     "clvm_tools==0.4.3",
     "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
-    "bitstring==3.1.7",  # Binary data management library
+    "bitstring==3.1.9",  # Binary data management library
     "colorlog==5.0.1",  # Adds color to logs
     "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
     "cryptography==3.4.7",  # Python cryptography library for TLS - keyring conflict
     "keyring==23.0.1",  # Store keys in MacOS Keychain, Windows Credential Locker
-    # Secure storage for keys on Linux (Will be replaced)
-    "keyrings.cryptfile==1.3.4",
+    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
     #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
     #  See https://github.com/frispete/keyrings.cryptfile/issues/15
     "PyYAML==5.4.1",  # Used for config file format
     "setproctitle==1.2.2",  # Gives the spare processes readable names
-    "sortedcontainers==2.3.0",  # For maintaining sorted mempools
+    "sortedcontainers==2.4.0",  # For maintaining sorted mempools
     "websockets==8.1.0",  # For use in wallet RPC and electron UI
     "click==7.1.2",  # For the CLI
     "dnspython==2.1.0",  # Query DNS seeds
@@ -104,12 +103,12 @@ kwargs = dict(
     },
     package_data={
         "spare": ["pyinstaller.spec"],
-        "spare.wallet.puzzles": ["*.clvm", "*.clvm.hex"],
+        "": ["*.clvm", "*.clvm.hex", "*.clib", "*.clinc", "*.clsp", "py.typed"],
         "spare.util": ["initial-*.yaml", "english.txt"],
-        "spare.ssl": ["chia_ca.crt", "chia_ca.key", "dst_root_ca.pem"],
+        "spare.ssl": ["spare_ca.crt", "spare_ca.key", "dst_root_ca.pem"],
         "mozilla-ca": ["cacert.pem"],
     },
-    use_scm_version={"fallback_version": "unknown-no-.git-directory"},
+    version="1.0.0",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     zip_safe=False,
